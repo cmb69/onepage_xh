c6b42685ccf30e9e757dff1a1a16ee222ef05fbe
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-10 22:01:12 +0100

    actually added German localization

---
+++ languages/de.php
@@ -0,0 +1,13 @@
+<?php
+
+$plugin_tx['onepage']['menu_info']="Info";
+
+$plugin_tx['onepage']['alt_logo']="Yin und Yang";
+$plugin_tx['onepage']['alt_toplink']="Nach oben";
+
+$plugin_tx['onepage']['cf_admin_template']="Das Template für den Bearbeitungsmodus. Leer lassen, um das normale Template zu verwenden.";
+$plugin_tx['onepage']['cf_url_numeric']="Ob numerische URLs für die Seiten-Abschnitte verwendet werden sollen.";
+$plugin_tx['onepage']['cf_scroll_duration']="Die Dauer des sanften Scrollens (in Millisekunden). Leer lassen, um das sanfte Scrolling zu deaktivieren.";
+$plugin_tx['onepage']['cf_scroll_easing']="Die Beschleunigungsfunktion für das sanfte Scrollen.";
+
+?>
