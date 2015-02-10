14e6cfec06d6bb9573966ef72ddf6a91cbe332fe
Christoph M. Becker <cmbecker69@gmx.de>
2015-02-10 20:51:00 +0100

    added German localization

--- languages/en.php
+++ languages/en.php
@@ -2,12 +2,12 @@
 
 $plugin_tx['onepage']['menu_info']="Info";
 
-$plugin_tx['onepage']['alt_logo']="Yin and yang";
+$plugin_tx['onepage']['alt_logo']="Yin and Yang";
 $plugin_tx['onepage']['alt_toplink']="Scroll to top";
 
-$plugin_tx['onepage']['cf_admin_template']="The template for the admin mode. Leave empty to use the normal template.";
+$plugin_tx['onepage']['cf_admin_template']="The template for the edit mode. Leave empty to use the normal template.";
 $plugin_tx['onepage']['cf_url_numeric']="Whether to use numeric URLs for the page sections.";
-$plugin_tx['onepage']['cf_scroll_duration']="The duration of the soft scrolling (milliseconds). Leave empty to disable smooth scrolling.";
+$plugin_tx['onepage']['cf_scroll_duration']="The duration of the smooth scrolling (milliseconds). Leave empty to disable smooth scrolling.";
 $plugin_tx['onepage']['cf_scroll_easing']="The easing function of the smooth scrolling.";
 
 ?>
