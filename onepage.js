cmb69/onepage_xh

commit 35b67ac97e60cd72f86de40be8ac798f73f9e9c2

Close #11: Make scroll duration dependent on scroll amount
We reinterpret the scroll→duration as *maximum* duration of the
scrolling. The maximum duration is only applied when scrolling the
whole document (top to bottom or vice versa). If only a fraction of the
document has to be scrolled, the scrolling endures only this fraction
of the maximum duration.

We also appropriately increase the default value.

onepage.js
@@ -154,12 +154,12 @@
         }
 
         master = masterElement;
-        duration = ONEPAGE.scrollDuration;
         element = document.getElementById(id);
         endOffset = element ? getElementTop(element) : 0;
         if (typeof window.requestAnimationFrame !== "undefined") {
             oldOffset = startOffset = master.scrollTop;
             delta = endOffset - startOffset;
+            duration = ONEPAGE.scrollDuration * Math.abs(delta) / master.scrollHeight;
             window.requestAnimationFrame(step);
         } else {
             master.scrollTop = endOffset;

config/config.php
@@ -3,7 +3,7 @@
 $plugin_cf['onepage']['admin_template']="";
 $plugin_cf['onepage']['url_numeric']="";
 $plugin_cf['onepage']['use_javascript']="true";
-$plugin_cf['onepage']['scroll_duration']="300";
+$plugin_cf['onepage']['scroll_duration']="600";
 $plugin_cf['onepage']['scroll_easing']="sine";
 $plugin_cf['onepage']['inner_class']="onepage_page_inner";
 

languages/de.php
@@ -8,7 +8,7 @@ $plugin_tx['onepage']['alt_toplink']="Nach oben";
 $plugin_tx['onepage']['cf_admin_template']="Das Template für den Bearbeitungsmodus. Leer lassen, um das normale Template zu verwenden.";
 $plugin_tx['onepage']['cf_url_numeric']="Ob numerische URLs für die Seiten-Abschnitte verwendet werden sollen.";
 $plugin_tx['onepage']['cf_use_javascript']="Ob das mitgelieferte JavaScript verwendet werden soll oder nicht.";
-$plugin_tx['onepage']['cf_scroll_duration']="Die Dauer des sanften Scrollens (in Millisekunden). Leer lassen, um das sanfte Scrolling zu deaktivieren.";
+$plugin_tx['onepage']['cf_scroll_duration']="Die maximale Dauer des sanften Scrollens (in Millisekunden). Leer lassen, um das sanfte Scrolling zu deaktivieren.";
 $plugin_tx['onepage']['cf_scroll_easing']="Die Beschleunigungsfunktion für das sanfte Scrollen.";
 $plugin_tx['onepage']['cf_inner_class']="Die CSS-Klasse, die für den inneren Seiten-Container gesetzt werden soll."
 

languages/en.php
@@ -8,7 +8,7 @@ $plugin_tx['onepage']['alt_toplink']="Scroll to top";
 $plugin_tx['onepage']['cf_admin_template']="The template for the edit mode. Leave empty to use the normal template.";
 $plugin_tx['onepage']['cf_url_numeric']="Whether to use numeric URLs for the page sections.";
 $plugin_tx['onepage']['cf_use_javascript']="Whether to use the shipped JavaScript or not.";
-$plugin_tx['onepage']['cf_scroll_duration']="The duration of the smooth scrolling (milliseconds). Leave empty to disable smooth scrolling.";
+$plugin_tx['onepage']['cf_scroll_duration']="The maximum duration of the smooth scrolling (milliseconds). Leave empty to disable smooth scrolling.";
 $plugin_tx['onepage']['cf_scroll_easing']="The easing function of the smooth scrolling.";
 $plugin_tx['onepage']['cf_inner_class']="The CSS class to set for the inner page container."
 

commit 14399227235d1563f58cf0888f7edb9d15281b3b

Fix regression introduced by commit 35b67ac9
As of this commit every other click on the menu scrolled to the top
instead of the page. This was caused by a division by zero (resulting
in NaN) in case no scrolling was required. We catch that early to avoid
even requesting an animation frame.

onepage.js
@@ -160,7 +160,9 @@
             oldOffset = startOffset = master.scrollTop;
             delta = endOffset - startOffset;
             duration = ONEPAGE.scrollDuration * Math.abs(delta) / master.scrollHeight;
-            window.requestAnimationFrame(step);
+            if (duration) {
+                window.requestAnimationFrame(step);
+            }
         } else {
             master.scrollTop = endOffset;
         }
@@ -176,9 +178,9 @@
         var master, scrollTop;
 
         master = masterElement;
-        scrollTop = master.scrollTop;
-        location.hash = id;
-        master.scrollTop = scrollTop;
+            scrollTop = master.scrollTop;
+            location.hash = id;
+            master.scrollTop = scrollTop;
         scrollToId(decodeURIComponent(id));
     }
 