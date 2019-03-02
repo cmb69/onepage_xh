4a718cd11e4665a640de9a5d718d6bd3a8e3425d
Christoph M. Becker <cmbecker69@gmx.de>
2019-03-02 14:47:52 +0100

    Close #37: Rid direct access prevention/version check

--- index.php
+++ index.php
@@ -19,23 +19,6 @@
  * along with Onepage_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Prevent direct access and usage from unsupported CMSimple_XH versions.
- */
-if (!defined('CMSIMPLE_XH_VERSION')
-    || strpos(CMSIMPLE_XH_VERSION, 'CMSimple_XH') !== 0
-    || version_compare(CMSIMPLE_XH_VERSION, 'CMSimple_XH 1.6.3', 'lt')
-) {
-    header('HTTP/1.1 403 Forbidden');
-    header('Content-Type: text/plain; charset=UTF-8');
-    die(
-        <<<EOT
-Onepage_XH detected an unsupported CMSimple_XH version.
-Uninstall Onepage_XH or upgrade to a supported CMSimple_XH version!
-EOT
-    );
-}
-
 const ONEPAGE_VERSION = '1.0beta2';
 
 /**
